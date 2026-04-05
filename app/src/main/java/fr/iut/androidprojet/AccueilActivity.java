02acd9d6a889ade8e651e4591afeca3dd4ff27a0
Galiano Enzo <enzo.galiano@etu.univ-grenoble-alpes.fr>
2026-04-05 10:02:08 +0200

    Premier commit : ajout du projet existant

---
+++ app/src/main/java/fr/iut/androidprojet/AccueilActivity.java
@@ -0,0 +1,53 @@
+package fr.iut.androidprojet;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+public class AccueilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_accueil);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button btnMaths = findViewById(R.id.btn_maths_exercices);
+        // Button btnMaths = findViewById(R.id.btn_culture_general_exercices);
+
+
+        // exercies maths / geometrie
+        btnMaths.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AccueilActivity.this, ListExercicesMathsActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        // exercies culture général
+        btnMaths.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AccueilActivity.this, ListExercicesMathsActivity.class);
+
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}
